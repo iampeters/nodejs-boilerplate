e2a524d6e2fc31fd02b1f677b4372bb4b488787d
Peters Chikezie <chikeziepeters@gmail.com>
2018-10-15 20:35:17 +0100

    Update routes.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,33 +2,11 @@
 const bodyParser = require('body-parser')
 const models = require('../models/users')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var jsonParser = bodyParser.json();
 
 module.exports = (app) => {
 	app.get('/', (req, res) => {
 		res.render('index')
 	})
-	
-	//get register
-	app.get('/register', (req, res) => {
-		res.render('register')
-	})
 
-	app.get('/register-success', (req, res) => {
-		res.render('register-success')
-	})
-	
-	
-	
-	app.get('/profile/:id', (req, res) => {
-		res.render('profile', {data: req.params.id})
-	})
-
-	//Login
-	app.get('/signin', (req, res) => {
-		res.render('signin')
-	})
-
-	
-
-	
 }
